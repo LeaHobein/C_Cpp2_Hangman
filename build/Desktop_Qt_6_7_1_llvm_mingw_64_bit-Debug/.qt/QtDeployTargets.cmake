f7f4be5bc2015309324380307c1ab417d4f39693
LOEA <lea.hobein@gmail.com>
2024-06-13 19:03:54 +0200

    FIX! user paths

--- build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_hangman_FILE C:/Users/leaho/OneDrive/Dokumente/hangman/build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/hangman.exe)
-set(__QT_DEPLOY_TARGET_hangman_RUNTIME_DLLS D:/qt/6.7.1/llvm-mingw_64/bin/Qt6Widgets.dll;D:/qt/6.7.1/llvm-mingw_64/bin/Qt6Gui.dll;D:/qt/6.7.1/llvm-mingw_64/bin/Qt6Core.dll)
